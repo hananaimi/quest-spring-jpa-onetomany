b4da098a94324e46f29cdc312dfb29319974c235
Hana Naimi <naimihanna@gmail.com>
2023-06-21 15:23:43 +0200

    challenge

--- src/main/java/com/wildcodeschool/wildandwizard/WildAndWizardApplication.java
+++ src/main/java/com/wildcodeschool/wildandwizard/WildAndWizardApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WildAndWizardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WildAndWizardApplication.class, args);
+
+ 		SpringApplication.run(WildAndWizardApplication.class, args);
 	}
 
 }
